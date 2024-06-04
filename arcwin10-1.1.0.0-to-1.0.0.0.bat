13ace77c65080ab5a0e1f7b9a65fc4ca717e2366
devz3ro <devz3ro@gmail.com>
2024-06-04 12:10:42 -0400

    Update arcwin10-1.1.0.0-to-1.0.0.0.bat

--- arcwin10-1.1.0.0-to-1.0.0.0.bat
+++ arcwin10-1.1.0.0-to-1.0.0.0.bat
@@ -6,7 +6,7 @@ IF %ERRORLEVEL% NEQ 0 (
     EXIT /B
 )
 
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%~dp0arcwin10-1.0.1.0-to-1.0.0.0.ps1'"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%~dp0arcwin10-1.1.0.0-to-1.0.0.0.ps1'"
 
 ECHO Press Enter to exit ...
 SET /P =
